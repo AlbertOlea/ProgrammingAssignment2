7f78a0b90cbdd2271e4f0fdf6f0d0f4c98d8355c
AlbertOlea <aolea2@uic.edu>
2019-09-08 20:33:37 -0500

    Grammatical changes made to both functions in the code. Descriptions were also added to both functions.

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -27,7 +27,9 @@ cachemean <- function(x, ...) {
 }
 
 
-##Answer (not much changed)
+##Answer (not much changed) x has been turned into a matrix from its previous numeric identity. The first function 
+##creates a matrix where 1. the value of the vector is set, 2. we get the value of the vector, 3. the value of the 
+##inverse is set, and 4. we get the value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -36,21 +38,25 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+##here we calculate the inverse of the matrix made with the above function. It first checks to see if the mean has already
+##been calculated. If it has been calculated, this function retrieves it from the cache. If not, calculates the mean and
+##sets that mean value in the cache using the setmean function.
+
 cacheSolve <- function(x, ...) {
-  m <- x$getmean()
+  m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
   m <- solve(data, ...)
-  x$setmean(m)
+  x$setinverse(m)
   m
 }
